619e767138a28ee08b4a6da7fe80f8a21c235913
Alperen Köylü <alperenkoylu@gmail.com>
2017-12-11 16:17:12 +0200

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -12,6 +12,10 @@ typedef enum { false, true } bool;
 int array1[SIZE];
 int array2[SIZE];
 int array3[SIZE];
+int array4[SIZE];
+int array5[SIZE];
+int array5b[SIZE];
+
 bool sorted = true;
 
 float processTime(struct timeval t0, struct timeval t1);
@@ -115,13 +119,86 @@ int bubbleSort() {
     printf("%f ms\n", processTime(tStart, tENd));
 }
 
+int partition(int* a, int low, int high);
+int quickSort(int* a, int low, int high) {
+	int pivot = 0;
+
+	if(low < high)
+	{
+		pivot = partition(a, low, high);
+		quickSort(a, low, pivot-1);
+		quickSort(a, pivot+1, high);
+	}		
+}
+
+    int partition(int* a, int low, int high)
+    {
+	    int pivot = a[high];
+	    int i, j;
+	    int tmp;
+
+	    j = low;
+
+	    for (i=low; i<high; i++)
+	    {
+		    if (a[i] <= pivot)
+		    {
+			    tmp = a[i];
+			    a[i] = a[j];
+			    a[j] = tmp;
+			    j++;
+		    }
+	    }
+
+	    tmp = a[j];
+	    a[j] = a[high];
+	    a[high] = tmp; 
+
+	    return j;
+    }
+
+void merging(int low, int mid, int high) {
+   int l1, l2, i;
+
+   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
+      if(array5[l1] <= array5[l2])
+         array5b[i] = array5[l1++];
+      else
+         array5b[i] = array5[l2++];
+   }
+   
+   while(l1 <= mid)    
+      array5b[i++] = array5[l1++];
+
+   while(l2 <= high)   
+      array5b[i++] = array5[l2++];
+
+   for(i = low; i <= high; i++)
+      array5[i] = array5b[i];
+}
+
+void mergeSort(int low, int high) {
+   int mid;
+   
+   if(low < high) {
+      mid = (low + high) / 2;
+      mergeSort(low, mid);
+      mergeSort(mid+1, high);
+      merging(low, mid, high);
+   } else { 
+      return;
+   }   
+}
+
 int main() {
     srand(time(NULL));
     
     for(int i = 0; i<SIZE; i++) {
-        array1[i] = rand()%100;
+        array1[i] = rand()%SIZE;
         array2[i] = array1[i];
         array3[i] = array1[i];
+        array4[i] = array1[i];
+        array5[i] = array1[i];
     }    
     
     printf("\t         ARRAY : "); printArray();
@@ -129,6 +206,20 @@ int main() {
     printf("\tSELECTION SORT : "); selectionSort();
     printf("\tINSERTION SORT : "); insertionSort();
     printf("\t   BUBBLE SORT : "); bubbleSort();
+
+    struct timeval tStartQ;
+	struct timeval tENdQ;
+    gettimeofday(&tStartQ, 0);
+    printf("\t    QUICK SORT : "); quickSort(array4, 0, SIZE);
+    gettimeofday(&tENdQ, 0);
+    printf("%f ms\n", processTime(tStartQ, tENdQ));
+
+    struct timeval tStartM;
+	struct timeval tENdM;
+    gettimeofday(&tStartM, 0);
+    printf("\t    MERGE SORT : "); mergeSort(0, SIZE);
+    gettimeofday(&tENdM, 0);
+    printf("%f ms\n", processTime(tStartM, tENdM));
 }
 
 float processTime(struct timeval t0, struct timeval t1)
